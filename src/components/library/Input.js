679596151b84df9a4930312987d3b0e9b498be5e
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-03-22 19:33:30 +0530

    Inputs section added

--- src/components/library/Input.js
+++ src/components/library/Input.js
@@ -1,17 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Input = () => {
-    return (
-        <div className="input-page container">
+  return (
+    <div className="input-page container">
+      <h1>Inputs</h1>
+      <p>Note: Inputs by default have outline ,border as none.</p>
 
-            <h1>Inputs</h1>
-            <p>Note: Inputs by default have outline ,border as none.</p>
-        
-        <input className="input" type="text"/>
-        
-        
-        </div>
-    )
-}
+      <h3 className="mtb1-rem">1. Simple inputs</h3>
+      <h3 className="h3 mtb1-rem">Email input</h3>
+      <input className="input" type="email" placeholder="Email..." />
+      <br />
+      <h3 className="h3 mtb1-rem">Text input</h3>
+      <input className="input" type="text" placeholder="Enter here..." />
+      <br />
+      <h3 className="h3 mtb1-rem">Password input</h3>
+      <input className="input" type="password" placeholder="Password" />
+
+      <SyntaxHighlighter
+        language="css"
+        style={dracula}
+      >{`<input class="input" type="email" placeholder="Email..." />
+<br />
+<h3 class="h3 mtb1-rem">Text input</h3>
+<input class="input" type="text" placeholder="Enter here..." />
+<br />
+<h3 class="h3 mtb1-rem">Password input</h3>
+<input class="input" type="password" placeholder="Password" />`}</SyntaxHighlighter>
+
+      <h3 className="mtb1-rem">1. Not so simple inputs</h3>
+      <h3 className="h3 mtb1-rem">Email input</h3>
+      <input className="input input-red" type="email" placeholder="Email..." />
+      <br />
+      <h3 className="h3 mtb1-rem">Text input</h3>
+      <input
+        className="input input-green"
+        type="text"
+        placeholder="Enter here..."
+      />
+      <br />
+      <h3 className="h3 mtb1-rem">Password input</h3>
+      <input
+        className="input input-blue"
+        type="password"
+        placeholder="Password"
+      />
+
+      
+<SyntaxHighlighter
+        language="css"
+        style={dracula}
+>{`<input class="input input-red" type="email" placeholder="Email..." />
+<br />
+<h3 class="h3 mtb1-rem">Text input</h3>
+<input class="input input-green" type="text" placeholder="Enter here..." />
+<br />
+<h3 class="h3 mtb1-rem">Password input</h3>
+<input class="input input-blue" type="password" placeholder="Password" />`}</SyntaxHighlighter>
+    </div>
+  );
+};
 
 export default Input;
